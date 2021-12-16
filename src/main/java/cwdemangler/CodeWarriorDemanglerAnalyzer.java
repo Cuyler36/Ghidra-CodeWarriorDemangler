d0fadc2c2b524df317ec8c7c9458bc89c32b1919
Cuyler36 <jeremy96@vt.edu>
2021-12-16 01:16:54 -0500

    Add Demangler Analyzer
    Allows proper interface option to appear under Analysis.

---
+++ src/main/java/cwdemangler/CodeWarriorDemanglerAnalyzer.java
@@ -0,0 +1,38 @@
+package cwdemangler;
+
+import ghidra.app.plugin.core.analysis.AbstractDemanglerAnalyzer;
+import ghidra.app.util.demangler.*;
+import ghidra.app.util.importer.MessageLog;
+import ghidra.program.model.listing.Program;
+
+/***
+ * 
+ * Demangler analyzer for CodeWarrior symbols.
+ */
+public class CodeWarriorDemanglerAnalyzer extends AbstractDemanglerAnalyzer {
+
+    private static final String NAME = "Demangle CodeWarrior";
+    private static final String DESCRIPTION =
+        "After a function is created, this analyzer will attempt to demangle " +
+            "the name and apply datatypes to parameters.";
+
+    private CodeWarriorDemangler demangler = new CodeWarriorDemangler();
+    
+    public CodeWarriorDemanglerAnalyzer() {
+        super(NAME, DESCRIPTION);
+        setDefaultEnablement(true);
+    }
+
+    @Override
+    public boolean canAnalyze(Program program) {
+        return demangler.canDemangle(program);
+    }
+
+    @Override
+    protected DemangledObject doDemangle(String mangled, DemanglerOptions options, MessageLog log)
+            throws DemangledException {
+        DemangledObject demangled = demangler.demangle(mangled, options);
+        return demangled;
+    }
+
+}
